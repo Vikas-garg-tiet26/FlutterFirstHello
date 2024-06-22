357cc19cf849e8b21d68f1f34cd06c04b4973412
Vikas-garg-tiet26 <vgarg1_be22@thapar.edu>
2024-06-22 15:59:45 +0530

    splashscreen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,12 +1,58 @@
+// import 'dart:ui';
 import 'package:flutter/material.dart';
+// import 'package:flutter/widgets.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+                decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+            colors: [
+              Color.fromARGB(255, 206, 164, 231),
+              Color.fromRGBO(255, 255, 255, 1),
+            ],
+          ),
+         
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Image.asset(
+              'assets/1.png',
+             
+            ),
+            const SizedBox(
+                
+                ),
+            TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 122, 80, 191),
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8))),
+                  padding: const EdgeInsets.only(
+                      left: 70, top: 10, bottom: 10, right: 70)),
+              child: const Text(
+                'Get Started',
+                style: TextStyle(
+                    fontSize: 21,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
